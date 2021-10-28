2448c168ea6f1fce562673fbe76c244ad30c50cd
mansoor202 <mansoor.ahmad@focustreck.com>
2021-10-28 16:36:26 +0500

    router.push

--- task1/pages/sections.js
+++ task1/pages/sections.js
@@ -1,6 +1,16 @@
+import { useRouter } from "next/dist/client/router"
+import { route } from "next/dist/server/router"
 function Section(){
+    const router=useRouter()
+    const clickHandler=()=>{
+        router.push("/products")
+
+    }
     return(
+        <>
         <h2>Sections here </h2>
+        <button onClick={clickHandler}>Go to products</button>
+        </>
     )
 }
 export default Section
