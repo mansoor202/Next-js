7905e20609ab0f9f738c0a6d4834cbc5ceece555
mansoor202 <mansoor.ahmad@focustreck.com>
2021-10-28 15:52:40 +0500

    moving around routes from within the UI

--- task1/pages/index.js
+++ task1/pages/index.js
@@ -1,6 +1,17 @@
+import Link from 'next/link'
+
 function SayHello(){
     return(
+        <>
+        <Link href="/sections">
+            <button>Go to Sections</button>
+        </Link>
+
+        <Link href="/teams">
+            <a>Teams</a>
+        </Link>
         <h2>Hello</h2>
+        </>
     )
 }
 
--- task1/pages/teams/index.js
+++ task1/pages/teams/index.js
@@ -1,6 +1,14 @@
+import Link from "next/dist/client/link"
 function Teams(){
+
+
     return(
+        <>
+        <Link href="/">
+            <a>go to homepage</a>
+        </Link>
         <h2>Teams Here </h2>
+        </>
     )
 }
 export default Teams
